e189ff4d8d6e0a8f09376b3aeb2fbe828741eacd
Prince <princeinsan1919@gmail.com>
2020-11-08 13:04:05 +0530

    initial commit

---
+++ IELTSManagement/src/main/java/com/example/scm/IeltsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeltsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeltsManagementApplication.class, args);
+	}
+
+}
